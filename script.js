1832dfc9fcf4356335f0fa6fda747749ff4d09fc
Antonio Molinari <molinari@incode.it>
2024-12-20 18:00:22 +0100

    fixes

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const buttonclick = () => {
     console.log('Button clicked');
-    }
+}
 
-    const button = document.getElementById('cta-button');
-    btn1.addEventListener('click', buttonclick);
+const button = document.getElementById('cta-button');
+button.addEventListener('click', buttonclick);
+
+
+// get all a tags with "#" in href
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
