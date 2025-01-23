a9d0726125a6d1826052ac8e12f954cf9f4cd8ae
norbismarta <norbis.007441@laba.edu>
2025-01-23 12:35:50 +0100

    swiper

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM completamente caricato!');
 
+    const buttonclick = () => {
+        console.log('Button clicked');
+    };
 
-const buttonclick = () => {
-    console.log('Button clicked');
-}
+    const button = document.getElementById('cta-button');
+    if (button) { 
+        button.addEventListener('click', buttonclick);
+    }
 
-const button = document.getElementById('cta-button');
-button.addEventListener('click', buttonclick);
-
-
-// get all a tags with "#" in href
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
-        });
+         });
     });
 });
 
+    //  Swiper
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3, 
+    spaceBetween: 50, 
+    grabCursor: true, 
+    loop: true,
+    centeredSlides: true, 
+    pagination: {
+        delay: 3000, 
+        },
+    });
 });
